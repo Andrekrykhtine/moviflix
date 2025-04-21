d54eaeb382dbaa0344d77398a8da9bde5edfc936
Andre Krykhtine <andkryk@gmail.com>
2025-04-21 10:38:52 -0300

    feat: add express server with movies endpoint
    - Added express as a dependency in package.json
    - Created a basic express server in server.ts
    - Implemented a GET endpoint for '/movies' that returns a movie listing message
    - Configured the server to listen on port 3000

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,10 @@
-const test = 'hello world';
+import express from "express";
+
+const port = 3000;
+const app = express();
+app.get('/movies', (req, res) => {
+   res.send('Listagem de filmes ');
+});
+app.listen(port, () => {
+   console.log(`Servidor em execução em http://localhost:${port}`);
+});
