8a4603b36b1b1d626f843e482d502967e377503c
Andre Krykhtine <andkryk@gmail.com>
2025-04-22 14:57:55 -0300

    feat: adicionar endpoint POST para cadastro de filmes

--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,44 @@
 import express from "express";
 import { PrismaClient } from "@prisma/client";
 
+
 const port = 3000;
 const app = express();
 const prisma = new PrismaClient();
 
-app.get('/movies', async (_, res) => {
-   const movies = await prisma.movie.findMany({
-     orderBy: { title: 'asc' },
-     include: { genres: true,
-      languages: true },
-   });
-   res.json(movies);
-  });  
+app.use(express.json());
 
+app.get("/movies", async (_, res) => {
+  const movies = await prisma.movie.findMany({
+    orderBy: { title: "asc" },
+    include: { genres: true, languages: true },
+  });
+  res.json(movies);
+});
+
+app.post("/movies", async (req, res) => {
+
+   // console.log(`conteudo do body enviado na req: ${req.body}`); 
+   const { title, genre_id, language_id, oscar_count, release_date } = req.body;
+
+try{
+     await prisma.movie.create({
+    data: {
+      title: title,
+      genre_id: genre_id,
+      language_id: language_id,
+      oscar_count: oscar_count,
+      release_date: new Date(release_date),
+    },
+  });
+}
+catch(error){
+   console.error("Error while creating a movie:", error);
+   return res.status(500).send({mensagem:"falha ao cadastrar um filme"});
+}
+
+  res.status(201).send();
+});
 app.listen(port, () => {
-   console.log(`Servidor em execução em http://localhost:${port}`);
+  console.log(`Servidor em execução em http://localhost:${port}`);
 });
