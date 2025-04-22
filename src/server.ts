0c95a0a89e1a64c315325a6e195d6dcfdc0e59bd
Andre Krykhtine <andkryk@gmail.com>
2025-04-22 10:57:23 -0300

    feat: add Prisma schema and integrate Prisma Client for movie retrieval
    - Created a new Prisma schema defining models for genres, languages, and movies.
    - Added TypeScript definitions and JavaScript implementation for Prisma Client.
    - Updated server to use Prisma Client for fetching movies from the database.

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,19 @@
 import express from "express";
+import { PrismaClient } from "@prisma/client";
 
 const port = 3000;
 const app = express();
-app.get('/movies', (req, res) => {
-   res.send('Listagem de filmes ');
-});
+const prisma = new PrismaClient();
+
+app.get('/movies', async (_, res) => {
+   const movies = await prisma.movie.findMany({
+     orderBy: { title: 'asc' },
+     include: { genres: true,
+      languages: true },
+   });
+   res.json(movies);
+  });  
+
 app.listen(port, () => {
    console.log(`Servidor em execução em http://localhost:${port}`);
 });
